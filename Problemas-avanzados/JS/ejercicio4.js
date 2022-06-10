ee4a63fbc78d8400d8f9ac0de7f7a51add1e7e08
Juan Carlos Donado Vanegas <54679777+juancDonado@users.noreply.github.com>
2022-06-10 18:06:15 -0500

    Time Lapse

--- Problemas-avanzados/JS/ejercicio4.js
+++ Problemas-avanzados/JS/ejercicio4.js
@@ -3,5 +3,24 @@ function timeLapse(time1, time2){
   else return 60 - (time1 - time2);
 }
 
-let a,b;
+let a = parseInt(prompt('Digita el primer tiempo'));
+let b = parseInt(prompt('Digita el segundo tiempo'));
+let lapso = timeLapse(a,b);
+let c = parseInt(prompt('Digita otro tiempo'));
+let cierto = true;
+
+while(c != -1 && cierto){
+  if(isNaN(c)){
+    console.log('No es un numero');
+    break;
+  }else{
+    let nuevoLapso  = timeLapse(b,c);
+    cierto = lapso == nuevoLapso;
+    b = c;
+    c = parseInt(prompt('Digita otro tiempo'));
+  }
+}
+if(cierto) console.log(lapso);
+else console.log(0);
+
 
